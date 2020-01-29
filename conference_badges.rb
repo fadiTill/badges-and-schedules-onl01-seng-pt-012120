5a583b1c8d72b1edae91e767b19b86a417f496df
fadimata tillman <fadi.tillman@gmail.com>
2020-01-29 17:18:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+
+
+  def badge_maker(name)
+   return"Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map do |name|
+    badge_maker(name)
+    end
+end
+
+
+def assign_rooms(speackers)
+  speackers.map.with_index do |name,index|
+   "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
